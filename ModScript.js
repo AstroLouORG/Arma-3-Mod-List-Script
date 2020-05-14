d929ef883722089ed72b42f3833fc2442acb6a47
AstroLouORG <43894665+AstroLouORG@users.noreply.github.com>
2020-05-14 02:39:47 +0200

    Fixed output path

--- ModScript.js
+++ ModScript.js
@@ -1,12 +1,10 @@
 const fs = require("fs")
-
-let path = ("Arma 3 Server Path") // Change this path, to the Server Folder, where the mod folders are located.
-
+let path = ("F:/Spiele/SteamGames/steamapps/common/Arma 3 Server") // Change this path, to the Server Folder, where the mod folders are located.
 let array = fs.readdirSync(path)
 let names = array.filter(name => name.startsWith("@"))
     fs.writeFileSync('modlist.txt', names.join(';'))
 
-console.log(`SUCCESS: modlist.txt Successfully Created in ${path}`)
+console.log(`SUCCESS: modlist.txt Successfully Created in ${process.cwd()}`)
 
 // Install node.js on your PC. Then run this script with "node ModScript.js"
 // It will create a file called "modlist.txt" to the path that you have given it.
