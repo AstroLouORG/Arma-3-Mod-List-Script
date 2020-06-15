54767a7da260e3ad074baedc3db6be6b0a4a16fd
AstroLouORG <43894665+AstroLouORG@users.noreply.github.com>
2020-06-15 02:49:11 +0200

    extended description

--- ModScript.js
+++ ModScript.js
@@ -1,14 +1,14 @@
 const fs = require("fs")
 
 
-let path = ("Arma 3 Server") // Change this path, to the Server Folder, where the mod folders are located.
+let path = ("Arma 3 Server") // Change this path, to the Server Folder(WITH FORWARD SLASHES), where the mod folders are located.
 
 
 let array = fs.readdirSync(path)
 let names = array.filter(name => name.startsWith("@"))
     fs.writeFileSync('modlist.txt', names.join(';'))
 
-console.log(`SUCCESS: modlist.txt Successfully Created in ${process.cwd()}modlist.txt`)
+console.log(`SUCCESS: modlist.txt Successfully Created in ${process.cwd()} modlist.txt`)
 
 // Install node.js on your PC. Then run this script with "node ModScript.js"
 // It will create a file called "modlist.txt" to the path that you have given it.
